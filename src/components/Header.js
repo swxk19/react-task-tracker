864b259392884dc18e38ecc50deea264df2d2f56
swxk19 <s.wxk19@gmail.com>
2022-05-10 15:37:00 +0800

    8

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = ({title, onAdd, showAdd}) => {
     return (
         <header className = 'header'> 
             <h1>{title}</h1>
-            {location.pathname === '/' && <Button 
+            {location.pathname === '/react-task-tracker' && <Button 
             color = {showAdd ? 'red' : 'green'}
             text = {showAdd ? 'hide' : 'poot'}
             onClick = {onAdd}/>}
